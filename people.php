eccac0fe9f4f9d9ad6ee7d3533e7f4da3f7c46df
Jean Pierre Taico <jt486@njit.edu>
2019-03-19 00:12:37 -0400

    polishing and fixing php/html coding

--- people.php
+++ people.php
@@ -2,22 +2,35 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>readContactCSV.php</title>
+    <title>My Contact List</title>
 </head>
 <body>
-<h1>Contacts</h1>
+<h1>My Contact List</h1>
 <div>
     <?php
     print <<< HERE
   <table border = "1">
   <tr>
-   <th>First</th>
-   <th>Last</th>
-   <th>email</th>
-   <th>phone</th>
+   <th>First Name</th>
+   <th>Last Name</th>
+   <th>E-mail</th>
+   <th>Contact Number</th>
   </tr>
 HERE;
-    
+    $data = file("data.csv");
+    foreach ($data as $line){
+        $lineArray = explode(",", $line);
+        list($firstName, $lastName, $email, $phone) = $lineArray;
+        print <<< HERE
+   <tr>
+   <td>$firstName</td>
+   <td>$lastName</td>
+   <td>$email</td>
+   <td>$phone</td>
+   </tr>
+HERE;
+    } // end foreach
+    print "</table>";
     ?>
 </div>
 </body>
