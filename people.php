9599ca5a30337e7833a0ff02644807159d38d8a2
Jean Pierre Taico <jt486@njit.edu>
2019-03-19 00:07:08 -0400

    edited people's names and added table code

---
+++ people.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>readContactCSV.php</title>
+</head>
+<body>
+<h1>Contacts</h1>
+<div>
+    <?php
+    print <<< HERE
+  <table border = "1">
+  <tr>
+   <th>First</th>
+   <th>Last</th>
+   <th>email</th>
+   <th>phone</th>
+  </tr>
+HERE;
+    
+    ?>
+</div>
+</body>
+</html>
